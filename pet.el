wyuenho/emacs-pet

commit 24fa6ed1ff0e55e2a409721cf3c9254ab0de52f2

Fix #1

pet.el
@@ -121,12 +121,15 @@ Otherwise, `project-root' is used."
                  (when-let ((root (car (project-roots project))))
                    (expand-file-name root)))))))
 
-(defun pet-find-file-from-project-root (file-name)
-  "Find FILE-NAME from project root.
+(defun pet-find-file-from-project-root (file)
+  "Find FILE from project root.
 
-Returns absolute path to FILE-NAME if found from in the root of
-the project, nil otherwise."
-  (car (directory-files (pet-project-root) t file-name)))
+FILE is a regular expression.
+
+Return absolute path to FILE if found from in the root of the
+project, nil otherwise."
+  (when-let ((root (pet-project-root)))
+    (car (directory-files root t file))))
 
 (defun pet-parse-json (str)
   "Parse JSON STR to an alist.  Arrays are converted to lists."
@@ -215,9 +218,9 @@ See `pet-watch-config-file' for details."
     `(progn
        (defvar ,cache-var nil)
        (defun ,name ()
-         (let* ((config-file (pet-find-file-from-project-root ,file-name))
-                (cache-kvp (and config-file
-                                (assoc config-file ,cache-var))))
+         (when-let* ((config-file (pet-find-file-from-project-root ,file-name))
+                     (cache-kvp (and config-file
+                                     (assoc config-file ,cache-var))))
            (if cache-kvp
                (cdr cache-kvp)
              (when config-file
@@ -423,7 +426,7 @@ Selects a virtualenv in the follow order:
 4. The `.venv' or `venv' directory in the project root if found.
 5. If the current project is using `pyenv', return the path to the virtualenv
    directory by looking up the prefix from `.python-version'."
-  (let ((root (pet-project-root)))
+  (when-let ((root (pet-project-root)))
     (cond ((alist-get root pet-project-virtualenv-cache nil nil 'equal))
           ((getenv "VIRTUAL_ENV")
            (expand-file-name (getenv "VIRTUAL_ENV")))
@@ -714,7 +717,7 @@ Delete configuration file caches and watchers when all
 `python-mode' buffers of a project have been closed."
   (when (and (buffer-file-name)
              (derived-mode-p 'python-mode))
-    (let ((root (pet-project-root)))
+    (when-let ((root (pet-project-root)))
       (when (null (cl-loop for buf in (buffer-list)
                            if (and (not (equal buf (current-buffer)))
                                    (string-prefix-p root (buffer-file-name buf)))

test/pet-test.el
@@ -51,10 +51,32 @@
     (expect 'minibuffer-message :not :to-have-been-called)))
 
 (describe "pet-project-root"
-  (it "should find project root"))
+  (it "should find project root with `projectile'"
+    (spy-on 'projectile-project-root :and-return-value "/")
+    (expect (pet-project-root) :to-equal "/"))
+
+  (it "should find project root with `project.el'"
+    (spy-on 'projectile-project-root :and-return-value nil)
+    (spy-on 'project-current :and-return-value '(vc . "/"))
+    (expect (pet-project-root) :to-equal "/"))
+
+  (it "should return nil when Python file does not appear to be in a project"
+    (spy-on 'projectile-project-root :and-return-value nil)
+    (spy-on 'project-current :and-return-value nil)
+    (expect (pet-project-root) :to-be nil)))
 
 (describe "pet-find-file-from-project-root"
-  (it "should find file from project root"))
+  (it "should find file from project root"
+    (spy-on 'pet-project-root :and-return-value "/etc")
+    (expect (pet-find-file-from-project-root "\\`passwd\\'") :to-equal "/etc/passwd"))
+
+  (it "should return nil when file not found from project root"
+    (spy-on 'pet-project-root :and-return-value "/etc")
+    (expect (pet-find-file-from-project-root "idontexist") :to-be nil))
+
+  (it "should return nil when not under a project"
+    (spy-on 'pet-project-root :and-return-value nil)
+    (expect (pet-find-file-from-project-root "foo") :to-be nil)))
 
 (describe "pet-parse-json"
   (it "should parse a JSON string to alist"))

commit db539bf38be3a9259e00ca4852a8de71031e477a

fix regression introduced in 24fa6ed

pet.el
@@ -219,9 +219,9 @@ See `pet-watch-config-file' for details."
     `(progn
        (defvar ,cache-var nil)
        (defun ,name ()
-         (when-let* ((config-file (pet-find-file-from-project-root ,file-name))
-                     (cache-kvp (and config-file
-                                     (assoc config-file ,cache-var))))
+         (let* ((config-file (pet-find-file-from-project-root ,file-name))
+                (cache-kvp (and config-file
+                                (assoc config-file ,cache-var))))
            (if cache-kvp
                (cdr cache-kvp)
              (when config-file