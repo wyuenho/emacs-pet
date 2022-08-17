179671cc15c2da3aaba7861c4d7b012c6444c72e
Jimmy Yuen Ho Wong <wyuenho@gmail.com>
2022-08-17 23:15:03 +0100

    test pet-pre-commit-config-has-hook-p

--- test/pet-test.el
+++ test/pet-test.el
@@ -431,8 +431,18 @@
       (expect (pet-use-pipenv-p) :to-be nil))))
 
 (describe "pet-pre-commit-config-has-hook-p"
-  (it "should return t if `.pre-commit-config.yaml' has hook declared")
-  (it "should return nil if `.pre-commit-config.yaml' does not have hook declared"))
+  :var ((pre-commit-config-content
+          '((repos
+              ((hooks ((id . "black"))) (repo . "https://github.com/psf/black") (rev . "22.6.0"))))))
+
+  (before-each
+    (spy-on 'pet-pre-commit-config :and-return-value pre-commit-config-content))
+
+  (it "should return t if `.pre-commit-config.yaml' has hook declared"
+    (expect (pet-pre-commit-config-has-hook-p "black") :to-be-truthy))
+
+  (it "should return nil if `.pre-commit-config.yaml' does not have hook declared"
+    (expect (pet-pre-commit-config-has-hook-p "isort") :not :to-be-truthy)))
 
 (describe "pet-parse-pre-commit-db"
   (it "should parse `pre-commit' database to alist"))
