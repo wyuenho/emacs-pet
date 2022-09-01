0317b4f3248eb60189e2e2ea5abab7f4d37534c5
Jimmy Yuen Ho Wong <wyuenho@gmail.com>
2022-09-01 17:35:56 +0100

    fill package commentary

--- pet.el
+++ pet.el
@@ -26,9 +26,9 @@
 
 ;;; Commentary:
 
-;; __P__ython __E__xecutable __T__racker.  Tracks downs the correct Python executables
-;; from the various virtualenv management tools and assign them to buffer local
-;; variables.  The package to end all Emacs virtualenv packages.
+;; __P__ython __E__xecutable __T__racker.  Tracks downs the correct Python
+;; executables from the various virtualenv management tools and assign them to
+;; buffer local variables.  The package to end all Emacs virtualenv packages.
 
 ;;; Code:
 
