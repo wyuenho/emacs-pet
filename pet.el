wyuenho/emacs-pet

commit b6b80979fd0632917180befb92eb6a63d520a463

better formatting

pet.el
@@ -1348,25 +1348,26 @@ has assigned to."
             kvp)
 
       (insert (propertize (format "%-40s" "apheleia-formatters:")
-                          'face 'font-lock-variable-name-face) "\n")
-      (insert
-       (pp-to-string
-        (with-current-buffer buf
-          (if (boundp 'apheleia-formatters)
-              (mapcar (lambda (key) (alist-get key apheleia-formatters))
-                      '(black isort ruff ruff-isort yapf))
-            'unbound))))
+                          'face 'font-lock-variable-name-face))
+      (apply 'insert
+             (with-current-buffer buf
+               (if (boundp 'apheleia-formatters)
+                   (list "\n"
+                         (pp-to-string
+                          (mapcar (lambda (key) (alist-get key apheleia-formatters))
+                                  '(black isort ruff ruff-isort yapf))))
+                 (list (format "%s\n" 'unbound)))))
 
       (insert (propertize (format "%-40s" "format-all--executable-table:")
-                          'face 'font-lock-variable-name-face) "\n")
-      (insert
-       (pp-to-string
-        (with-current-buffer buf
-          (if (boundp 'format-all--executable-table)
-              (mapcar (lambda (key)
-                        (cons key (gethash key format-all--executable-table)))
-                      '(black isort ruff yapf))
-            'unbound))))
+                          'face 'font-lock-variable-name-face))
+      (apply 'insert
+             (with-current-buffer buf
+               (if (boundp 'format-all--executable-table)
+                   (list "\n"
+                         (pp-to-string
+                          (mapcar (lambda (key) (alist-get key format-all--executable-table))
+                                  '(black isort ruff yapf))))
+                 (list (format "%s\n" 'unbound)))))
 
       (when (with-current-buffer buf (fboundp 'eglot--guess-contact))
         (insert (propertize (format "%-40s" "(eglot--guess-contact):") 'face 'font-lock-variable-name-face) "\n")

commit f652d7d797a26b0129296d9841e8716c7510c08b

Use gethash for format-all executable lookup
Previously the code used alist-get to retrieve entries from
format-all--executable-table. That variable is a hash table, so
alist-get fails. This change replaces alist-get with gethash to query
the hash table and restore correct lookup and output.

This was a bug introduced in b6b80979fd0632917180befb92eb6a63d520a463,
where gethash was replaced by alist-get.

pet.el
@@ -1804,7 +1804,7 @@ has assigned to."
                (if (boundp 'format-all--executable-table)
                    (list "\n"
                          (pp-to-string
-                          (mapcar (lambda (key) (alist-get key format-all--executable-table))
+                          (mapcar (lambda (key) (gethash key format-all--executable-table))
                                   '(black isort ruff yapf))))
                  (list (format "%s\n" 'unbound)))))
 